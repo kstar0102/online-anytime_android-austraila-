759c86f0e6854dc69c41f5c5fefe5ece3b6ae4fe
bluenike610 <bluenike0188@gmail.com>
2020-06-30 23:16:04 +0900

    login local db

--- app/src/main/java/com/austraila/online_anytime/activitys/LoginActivity.java
+++ app/src/main/java/com/austraila/online_anytime/activitys/LoginActivity.java
@@ -1,28 +1,70 @@
 package com.austraila.online_anytime.activitys;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.austraila.online_anytime.LocalManage.DatabaseHelper;
 import com.austraila.online_anytime.R;
 
 public class LoginActivity extends AppCompatActivity {
+    private SQLiteDatabase db;
+    private SQLiteOpenHelper openHelper;
+    EditText email, pass;
     Button loginBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
         getSupportActionBar().hide();
+
+        openHelper = new DatabaseHelper(this);
+        db = openHelper.getWritableDatabase();
+        insertData("1","1");
+
+        email = findViewById(R.id.login_email);
+        pass = findViewById(R.id.login_pass);
 
         loginBtn = findViewById(R.id.login_btn);
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
+                String Email = email.getText().toString().trim();
+                String password = pass.getText().toString().trim();
+                if (Email.isEmpty() || password.isEmpty()) {
+                    Toast.makeText(LoginActivity.this, "Enter your Email and Password to login", Toast.LENGTH_SHORT).show();
+                    return;
+                }else {
+                    Cursor cursor = db.rawQuery("SELECT *FROM " + DatabaseHelper.TABLE_NAME + " WHERE " + DatabaseHelper.COL_2 + "=? AND " + DatabaseHelper.COL_2 + "=?", new String[]{Email, password});
+                    if (cursor != null) {
+                        if (cursor.getCount() > 0) {
+                            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                            Toast.makeText(getApplicationContext(), "Login sucess", Toast.LENGTH_SHORT).show();
+                        }
+                        else {
+                            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(LoginActivity.this);
+                            // set title
+                            alertDialogBuilder.setTitle("エラー");
+                            // set dialog message
+                            alertDialogBuilder.setMessage("ログインに失敗しました。メールアドレスとパスワードを再入力し、再度ログインをお試しください。");
+                            // create alert dialog
+                            AlertDialog alertDialog = alertDialogBuilder.create();
+                            // show it
+                            alertDialog.show();
+                            Toast.makeText(getApplicationContext(), "Login error", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
             }
         });
     }
@@ -30,5 +72,13 @@ public class LoginActivity extends AppCompatActivity {
     public void foregetClick(View view) {
         Intent intent = new Intent(LoginActivity.this, ForgetActivity.class);
         startActivity(intent);
+    }
+
+    public void insertData(String fGmail,String fPassword){
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(DatabaseHelper.COL_2,fGmail);
+        contentValues.put(DatabaseHelper.COL_3,fPassword);
+
+        db.insert(DatabaseHelper.TABLE_NAME,null,contentValues);
     }
 }
