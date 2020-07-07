86e4f436d9ebb981e28ec5c42d7d97598511b0e0
bluenike610 <bluenike0188@gmail.com>
2020-07-07 21:01:40 +0900

    setting page fix and completed

--- app/src/main/java/com/austraila/online_anytime/activitys/SettingActivity.java
+++ app/src/main/java/com/austraila/online_anytime/activitys/SettingActivity.java
@@ -14,7 +14,9 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
@@ -22,30 +24,47 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.austraila.online_anytime.Common.Common;
 import com.austraila.online_anytime.LocalManage.DatabaseHelper;
 import com.austraila.online_anytime.R;
 import com.austraila.online_anytime.activitys.LoginDepartment.LoginActivity;
 import com.google.android.material.navigation.NavigationView;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SettingActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener  {
     private SQLiteDatabase db;
     private SQLiteOpenHelper openHelper;
     DrawerLayout settinglayout;
     private NavigationView navigation;
-    TextView setting_name, setting_email,setting_time,sidemenu_email,setting_username;
+    RelativeLayout loading;
+    TextView setting_name, setting_email,setting_time,sidemenu_email;
     ImageView side_menu_setting;
     Button sync_btn;
-    String useremail, username;
+    String useremail, username, userpass,result;
+    RequestQueue queue;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
 
         getSupportActionBar().hide();
+
+        loading = findViewById(R.id.loadingSetting);
 
         openHelper = new DatabaseHelper(this);
         db = openHelper.getWritableDatabase();
@@ -57,6 +76,7 @@ public class SettingActivity extends AppCompatActivity implements NavigationView
                 do{
                     useremail = cursor.getString(cursor.getColumnIndex("Gmail"));
                     username = cursor.getString(cursor.getColumnIndex("username"));
+                    userpass = cursor.getString(cursor.getColumnIndex("Password"));
                 }while(cursor.moveToNext());
             }
             cursor.close();
@@ -76,30 +96,73 @@ public class SettingActivity extends AppCompatActivity implements NavigationView
             @RequiresApi(api = Build.VERSION_CODES.N)
             @Override
             public void onClick(View v) {
-                setting_email.setText(useremail);
-                setting_name.setText(username);
+                loading.setVisibility(View.VISIBLE);
+                String url = Common.getInstance().getBaseURL() + Common.getInstance().getApiKey();
+                StringRequest postRequest = new StringRequest(Request.Method.POST, url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                System.out.println(response);
+                                JSONObject jsonObject = null;
+                                try {
+                                    jsonObject = new JSONObject(response);
+                                    result = jsonObject.getString("success");
+                                    if (result.equals("true")){
+                                        loading.setVisibility(View.GONE);
+                                        setting_email.setText(useremail);
+                                        setting_name.setText(username);
 
-                Date date = Calendar.getInstance().getTime();
-                DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
-                String strDate = dateFormat.format(date);
-                setting_time.setText(strDate);
+                                        Date date = Calendar.getInstance().getTime();
+                                        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+                                        String strDate = dateFormat.format(date);
+                                        setting_time.setText(strDate);
 
-                AlertDialog alertDialog = new AlertDialog.Builder(SettingActivity.this).create();
-                alertDialog.setTitle("Notivce");
-                alertDialog.setMessage("Data was transferred successfully.");
-                alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
-                        new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int which) {
-                                dialog.dismiss();
+                                        AlertDialog alertDialog = new AlertDialog.Builder(SettingActivity.this).create();
+                                        alertDialog.setTitle("Notivce");
+                                        alertDialog.setMessage("Data was transferred successfully.");
+                                        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                                                new DialogInterface.OnClickListener() {
+                                                    public void onClick(DialogInterface dialog, int which) {
+                                                        dialog.dismiss();
+                                                    }
+                                                });
+                                        alertDialog.show();
+                                    } else {
+                                        loading.setVisibility(View.GONE);
+                                        Toast.makeText(SettingActivity.this, "Oops, Request failed.", Toast.LENGTH_LONG).show();
+                                    }
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
                             }
-                        });
-                alertDialog.show();
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                loading.setVisibility(View.GONE);
+                                System.out.println(error);
+                                Toast.makeText(SettingActivity.this, "It is currently offline.", Toast.LENGTH_LONG).show();
+                            }
+                        }){
+                    @Override
+                    protected Map<String, String> getParams()
+                    {
+                        Map<String, String> params = new HashMap<String, String>();
+                        params.put("email", useremail);
+                        params.put("password", userpass);
+                        return params;
+                    }
+                };
+                queue = Volley.newRequestQueue(SettingActivity.this);
+                queue.add(postRequest);
+
             }
         });
 
         settinglayout = findViewById(R.id.setting_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        navigationView.bringToFront();
 
         findViewById(R.id.menu_btn_setting).setOnClickListener(new View.OnClickListener() {
             @Override
